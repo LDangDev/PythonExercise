780e917d41a95f82fd8d9151a6d536304f2c88f6
locphuocdang <dangloc2407@gmail.com>
2023-08-30 15:26:59 +0300

    update test.py

--- test.py
+++ test.py
@@ -3,17 +3,15 @@ import random
 
 names = set()
 while True:
-	user_input = input("Enter a name: ")
-	if(user_input == ''):
-		break
-	if (len(names) == 0):
-		print(f"New name")
-		names.add(user_input)
-	else:
-		if(user_input in names):
-			print(f"Existing name")
-		else:
-			print(f"New name")
-			names.add(user_input)
-
-		
+    user_input = input("Enter a name: ")
+    if user_input == "":
+        break
+    if len(names) == 0:
+        print(f"New name")
+        names.add(user_input)
+    else:
+        if user_input in names:
+            print(f"Existing name")
+        else:
+            print(f"New name")
+            names.add(user_input)
