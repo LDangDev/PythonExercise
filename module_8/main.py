04d9cb689f25236ad854948414f5da3e8ef855c1
locphuocdang <dangloc2407@gmail.com>
2023-09-01 19:18:08 +0300

    update ex 3

--- module_8/main.py
+++ module_8/main.py
@@ -1,6 +1,6 @@
 import mysql.connector
-
-
+import math
+from geopy import distance
 # 8. Using relational databases
 # Ex 1
 def getAirportNameAndLocation(ICAO_code):
@@ -36,7 +36,7 @@ getAirportNameAndLocation(ICAO_code)
 # Ex 2
 def getAreaCodeInput(area_code):
     upper_area_code = area_code
-    sql = "select ident, type, country.name as country_name, count(*) from airport "
+    sql = "select ident, type, country.name as country_name, count(*) as total from airport "
     sql += "inner join country on airport.iso_country = country.iso_country "
     sql += f"where country.iso_country = '{upper_area_code}' group by type;"
 
@@ -62,3 +62,34 @@ connection = mysql.connector.connect(
 )
 area_code_input = input("Enter the area code: ").upper()
 getAreaCodeInput(area_code_input)
+
+# Ex 3
+
+
+def calculate_airports_distance(first_ICAO_code, second_ICAO_code):
+    sql = f"select ident, name, latitude_deg , longitude_deg from airport where ident = '{first_ICAO_code}' or ident = '{second_ICAO_code}';"
+
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    response = cursor.fetchall()
+    first_airport = response[0][2], response[0][3]
+    second_airport = response[1][2], response[1][3]
+
+    airports_distance = distance.distance(first_airport, second_airport).km
+    print(f"The distance between {response[0][1]} and {response[1][1]} is {airports_distance:.2f} km")
+
+
+
+connection = mysql.connector.connect(
+    host="127.0.0.1",
+    port=3306,
+    database="flight_game",
+    user="dbuser",
+    password="pAs5w_0rD",
+    autocommit=True,
+)
+
+first_ICAO_code = input("Enter ICAO code of the first airport: ").upper()
+second_ICAO_code = input("Enter ICAO code of the second airport: ").upper()
+
+calculate_airports_distance(first_ICAO_code, second_ICAO_code)
